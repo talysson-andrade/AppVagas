7779c3fbaab4c31af76b22f5000175bc8dac29d5
Talysson Andrade <talysson.and@gmail.com>
2024-03-25 21:17:15 -0300

    teste2

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <LoginScreen />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
